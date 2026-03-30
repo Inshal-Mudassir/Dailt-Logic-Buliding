564f9721fd261f3876ed083b45b6173c24ad2ac3
Inshal <inshalmudassar715@gmail.com>
2026-03-30 20:10:32 +0500

    Q1-Two Sum

---
+++ Day1.js
@@ -0,0 +1,24 @@
+// 🧠 Problem 1: Two Sum 
+
+// Given an array of integers and a target value, return the indices of the two numbers that add up to the target.
+
+// Example:
+// Input: nums = [2, 7, 11, 15], target = 9  
+// Output: [0, 1]
+
+// let nums = [2, 7 ,11 ,15];
+// let target = 9;
+
+// let lenOfArray = nums.length;
+
+// for (let i=0; i<lenOfArray; i++) {
+//     for (let j=0; j<lenOfArray; j++) {
+//         if (i!== j && nums[i] + nums[j] == target) {
+//             console.log([i, j]);
+//             return;
+//         }
+//     }
+// }
+// console.log(result);
+// =======================================================================
+
